8f4e5a912e20b6488412e24e253f22a53ee5ebdb
nhloder <nhloder@gmail.com>
2019-10-09 12:16:37 -0600

    1dun2

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -3,74 +3,110 @@
 // Create a variable called 'myName' that's value is your name
 
 // Code here
-
+let myName = 'Nathan Loder'
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' that's value is your favorite number
 
-//Code here
+let faveNum = 420.69
 
 //////////////////PROBLEM 3////////////////////
 
 // Create a variable called 'lovesCode' and set it to true
 
 //Code here
+const lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, num1 and num2, and returns the sum of those two parameters.
 
-//Code here
+const sum = (num1, num2) => num1+num2;
 
 //////////////////PROBLEM 5////////////////////
 
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called x. Check to see if x is equal to true. If it is, return true as a boolean. If x does not equal true, return false as a boolean
 
 //Code here
-
+function lovesCodeChecker(x){
+	if (x !== true){
+		return false}
+	else (x === true);{
+		return true
+	}
+}
 //////////////////PROBLEM 6////////////////////
 
 // Create a function called 'oddChecker' that takes in one parameter, num. Check to see if the number is even or odd. If it is odd, return 'the number is odd' or return 'the number is even' if it is even.
 
 //Code here
+function oddChecker(num){
+	if (num % 2 ==0){
+		return 'the number is even'
+	}
+	else{
+		return 'the number is odd'
+	}
+}
 
 //////////////////PROBLEM 7////////////////////
 
 // Create a function called 'iLove' that takes in two parameters, name which will be a string  and love which will be a string of something you love. Have the function take the two parameters and retrun a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Bryan loves skateboarding"
 
 //Code Here
-
+function iLove(name, love){
+	return `${name} loves ${love}`
+}
 //////////////////PROBLEM 8////////////////////
 
 // Create a copy of the faveColors array called 'colorCopy' using slice
 const faveColors = ['red', 'green', 'black']
 
 //Code Here
-
+const colorCopy = faveColors.slice(0)
 //////////////////PROBLEM 9////////////////////
 
 // Add a fourth color to the end of the 'colorCopy' array using push
 
 //Code Here
-
+colorCopy.push('purple')
 //////////////////PROBLEM 10////////////////////
 
 // Create a new array called 'middleNums' from the numbers array that will capture only the middle numbers (2, 3, 4).
 const numbers = [1, 2, 3, 4, 5]
 
 //Code Here
-
+const middleNums = numbers.slice(1,4)
 //////////////////PROBLEM 11////////////////////
 
 // Create an object called 'me' that has these keys with the correct values: firstName, state, age, greeter. firstName will be your name as a string. state will be your current state or providence as a string. age will be your age as a number. greeter will be a method that returns the string 'Hello! My name is NAMEVALUE and I live in STATEVALUE' with the correct values
 
 //Code Here
+const me = {
+firstName: 'Nathan',
+state: 'constant distress',
+age:22,
+greeter: function greeter(){
+	return `Hello! my name is ${me.firstName} and I live in ${me.state} `
+}
+}
 
 //////////////////PROBLEM 12////////////////////
 
 // Create a function called 'bigOrSmall' that takes in one parameter, arr. Create a new array inside of bigOrSmall called 'answers'. Loop over the passed in arr param. If the number is GREATER than 100, push 'big' as a string to the answers array. If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. Return the answers array inside of the function
 
 // Code here
+function bigOrSmall(arr){
+	let answers = []
+	for (let i = arr.length ; i <= arr.length; i--){
+		if(arr.length[i] > 100){
+			answers.push('big')
+		}
+		else(arr.length[i] <= 100); {
+			answers.push('small')
+		}
+	} return answers
+}
 
 //////////////////PROBLEM 13////////////////////
 
